46c77dd93ffc9b9be83a990f9459db624168aa36
Evan hameed <evanhameed9966@gmail.com>
2020-07-08 22:58:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection,callback) {
 
+      for (let i =0 ; i<collection.length ;i++){
+        callback(collection[i]);
+      }
+      return collection
     },
 
-    map: function() {
+    map: function( collection,callback) {
+
+      let newArray = [];
+      for (let i = 0; i < collection.length; i++) {
+         newArray.push(callback(collection[i]))
+      }
+
+      return newArray;
 
     },
 
